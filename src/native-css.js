859e61a30b755642757488e222762628cb21a1ae
DoubleU23 <sf@tailored-digital.com>
2016-02-01 13:11:17 +0100

    nativeCSS.convertAsync() + readURLS (async)

--- src/native-css.js
+++ src/native-css.js
@@ -1,24 +1,30 @@
 'use strict';
 var packageJson = require('../package.json'),
 	lib = require('../lib'),
-	cssParser = require('css');
+	cssParser = require('css'),
+	fetchUrl = require('fetch').fetchUrl,
+	Promise = require('bluebird');
 
 var nativeCSS = function () {
-
+	this.isUrl = function (str) {
+		// feel free to use a better pattern
+		var pattern = new RegExp('^(?:(?:http|https|ftp)://)(?:\\S+(?::\\S*)?@)?(?:(?:(?:[1-9]\\d?|1\\d\\d|2[01]\\d|22[0-3])(?:\\.(?:1?\\d{1,2}|2[0-4]\\d|25[0-5])){2}(?:\\.(?:[0-9]\\d?|1\\d\\d|2[0-4]\\d|25[0-4]))|(?:(?:[a-z\\u00a1-\\uffff0-9]+-?)*[a-z\\u00a1-\\uffff0-9]+)(?:\\.(?:[a-z\\u00a1-\\uffff0-9]+-?)*[a-z\\u00a1-\\uffff0-9]+)*(?:\\.(?:[a-z\\u00a1-\\uffff]{2,})))|localhost)(?::\\d{2,5})?(?:(/|\\?|#)[^\\s]*)?$', 'i');
+		if (!pattern.test(str)) {
+			return false;
+		}
+		return true;
+	}
 	this.version = function () {
 		return ('native-css version: ' + packageJson.version)
 	}
-
 	this.help = function () {
 		return lib.readFile(__dirname + '/../docs/help.md')
 	}
-
 	this.indentObject = function (obj, indent) {
 		var self = this,
 			result = '';
 		return JSON.stringify(obj, null, indent || 0);
 	}
-
 	this.nameGenerator = function (name) {
 		name = name.replace(/\s\s+/g, ' ');
 		name = name.replace(/[^a-zA-Z0-9]/g, '_');
@@ -26,11 +32,9 @@ var nativeCSS = function () {
 		name = name.replace(/_+$/g, '');
 		return name;
 	}
-
 	this.mediaNameGenerator = function (name) {
 		return '@media ' + name;
 	}
-
 	this.transformRules = function (self, rules, result) {
 		var that = this;
 		rules.forEach(function (rule) {
@@ -61,6 +65,54 @@ var nativeCSS = function () {
 		this.transformRules(this, css.stylesheet.rules, result);
 		return result;
 	}
+
+
+	this.fetchUrlAsync = function (cssFile) {
+		return new Promise(function (resolve, reject) {
+			fetchUrl(cssFile, function (err, meta, body) {
+				if (err) throw err;
+				try {
+					resolve(body.toString());
+				} catch (err) {
+					reject(err);
+				}
+			});
+		});
+	}
+
+	this.convertAsync = function(cssFile) {
+		 var self = this,
+              path = process.cwd() + '/' + cssFile,
+              error;
+
+		return new Promise(function(resolve, reject) {
+			if (!self.isUrl(cssFile)) {
+				if ((require('fs').existsSync(path))) {
+					var css = lib.readFile(path);
+					css = cssParser.parse(css, {
+						silent: false,
+						source: path
+					});
+					return resolve(self.transform.apply(self, css));
+				} else {
+					reject(new Error('Ooops!\nError: CSS file not found!'));
+				}
+			}
+			else {
+				return self.fetchUrlAsync(cssFile)
+					.catch(function(err) {
+						reject(err);
+					})
+					.then(function (css) {
+						var css = cssParser.parse(css, {
+							silent: false,
+							source: path
+						});
+						resolve(self.transform(css));
+					});
+			}
+		});
+	};
 
 	this.convert = function (cssFile) {
 		var path = process.cwd() + '/' + cssFile;
