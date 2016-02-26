faf2121faee3ea4749287aed0efc6e0ca1f93a4c
DoubleU23 <sf@tailored-digital.com>
2016-02-26 12:32:32 +0100

    added tests for different input formats

--- test/conversion.js
+++ test/conversion.js
@@ -1,5 +1,6 @@
 var assert = require('assert'),
-	nativeCSS = require('../index.js');
+	nativeCSS = require('../index.js'),
+	lib = require('../lib/index');
 
 describe('Conversion', function() {
 	context('CSS -> React Style', function() {
@@ -110,8 +111,50 @@ describe('Conversion', function() {
 			});
 		});
 	});
-	context('Async', function() {
-		context('Read URL ... and convert', function() {
+	context('Input Formats', function() {
+		context('Accept String', function() {
+			it('should get a object without any error', function(done) {
+				var filename = 'test/fixtures/sample.css'
+				,	string = lib.readFile(filename)
+				,	result = nativeCSS.convert(string)
+				;
+				assert.equal(typeof result, 'object');
+				assert.deepEqual(result['a'], {"background": "#111", "color": "#000"});
+				assert.deepEqual(result['b'], {"background":"#222","color":"#111"});
+				assert.deepEqual(result['c'], {"color":"#000"});
+
+				done();
+			});
+		});
+		context('Accept Buffer', function() {
+			it('should get a object without any error', function(done) {
+				var filename = 'test/fixtures/sample.css'
+				,	string = lib.readFile(filename)
+				,	buffer = new Buffer(string)
+				,	result = nativeCSS.convert(buffer)
+				;
+				assert.equal(typeof result, 'object');
+				assert.deepEqual(result['a'], {"background": "#111", "color": "#000"});
+				assert.deepEqual(result['b'], {"background":"#222","color":"#111"});
+				assert.deepEqual(result['c'], {"color":"#000"});
+
+				done();
+			});
+		});
+		context('Accept Path', function() {
+			it('should get a object without any error', function(done) {
+				var fixture = 'test/fixtures/sample.css',
+					result = nativeCSS.convert(fixture);
+
+				assert.equal(typeof result, 'object');
+				assert.deepEqual(result['a'], {"background": "#111", "color": "#000"});
+				assert.deepEqual(result['b'], {"background":"#222","color":"#111"});
+				assert.deepEqual(result['c'], {"color":"#000"});
+
+				done();
+			});
+		});
+		context('Accept URL (async)', function() {
 			it('should get a object without any error', function(done, error) {
 				nativeCSS.convertAsync('http://raw.githubusercontent.com/raphamorim/native-css/master/test/fixtures/sample.css')
 					.then(function(result) {
